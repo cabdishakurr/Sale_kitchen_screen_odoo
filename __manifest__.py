258457f8ebdec50c62ab1d173f05334bb7f0f316
cabdishakurr <cabdishakurrrr@gmail.com>
2024-11-30 15:00:50 +0300

    a
    a

--- __manifest__.py
+++ __manifest__.py
@@ -20,54 +20,37 @@
 #
 #############################################################################
 {
-    'name': 'POS Kitchen Screen',
-    'version': '17.0.1.0.3',
-    'category': 'Point Of Sale',
-    'summary': 'POS Kitchen Screen facilitates sending certain orders '
-               'automatically to the kitchen.The POS Kitchen Screen allows for'
-               'the customization of order views, so that staff can see the '
-               'information that is most important to them.',
-    'description': 'The POS Kitchen Screen in Odoo 17 is a feature that '
-                   'allows restaurant staff to view and manage orders in '
-                   'real-time from the kitchen. This screen provides a clear '
-                   'and organized display of all active orders, enabling '
-                   'kitchen staff to prioritize and manage their tasks '
-                   'efficiently. The POS Kitchen Screen in Odoo 16 also '
-                   'allows for the customization of order views, so that '
-                   'staff can see the information that is most important to '
-                   'them. Additionally, this feature facilitates '
-                   'communication between front-end and back-end staff, '
-                   'enabling them to work together seamlessly and provide a '
-                   'better dining experience for customers.',
+    'name': 'Website Kitchen Screen',
+    'version': '17.0.1.0.0',
+    'category': 'Website',
+    'summary': 'Kitchen Screen for Website Orders',
+    'description': """
+        Website Kitchen Screen allows restaurant staff to view and manage 
+        orders from the website in real-time from the kitchen. This screen 
+        provides a clear and organized display of all active orders, enabling 
+        kitchen staff to prioritize and manage their tasks efficiently.
+    """,
     'author': 'Cybrosys Techno Solutions',
     'company': 'Cybrosys Techno Solutions',
     'maintainer': 'Cybrosys Techno Solutions',
     'website': "https://www.cybrosys.com",
-    'depends': ['pos_restaurant'],
+    'depends': [
+        'website_sale',
+        'sale_management',
+    ],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
         'data/kitchen_screen_sequence_data.xml',
         'views/sale_order_views.xml',
         'views/kitchen_screen_views.xml',
-        'views/pos_kitchen_screen_odoo_menus.xml',
-        'views/pos_order_views.xml',
+        'views/website_kitchen_screen_menus.xml',
     ],
     'assets': {
-        'point_of_sale._assets_pos': [
-            'pos_kitchen_screen_odoo/static/src/js/fields_load.js',
-            'pos_kitchen_screen_odoo/static/src/js/order_payment.js',
-            'pos_kitchen_screen_odoo/static/src/js/order_button.js',
-        ],
         'web.assets_backend': [
-            'pos_kitchen_screen_odoo/static/src/css/kitchen_screen.css',
-            'pos_kitchen_screen_odoo/static/src/js/kitchen_screen.js',
-            'pos_kitchen_screen_odoo/static/src/xml/kitchen_screen_templates.xml',
-            'https://code.jquery.com/jquery-1.10.2.min.js',
-            'https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js',
-            'https://fonts.googleapis.com',
-            'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js',
-            'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js',
+            'website_kitchen_screen/static/src/css/kitchen_screen.css',
+            'website_kitchen_screen/static/src/js/kitchen_screen.js',
+            'website_kitchen_screen/static/src/xml/kitchen_screen_templates.xml',
         ],
     },
     'images': [
@@ -76,5 +59,5 @@
     'license': 'LGPL-3',
     'installable': True,
     'auto_install': False,
-    'application': False,
+    'application': True,
 }
